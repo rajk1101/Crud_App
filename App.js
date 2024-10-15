c8fe733867f7f835e0a3c9dd744ba47047f16aad
Rajkamal <110493358+rajk1101@users.noreply.github.com>
2024-10-15 15:20:49 +0530

    Crud_Practice

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
+import { useState } from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import Header from './components/header.js'
+import TodoItem from './components/todoItem.js';
+import AddTodo from './components/addTodo.js'
 export default function App() {
+  const [todos,setTodos]=useState([
+    {text:'Buy coffee',key:'1'},
+    {text:'Create an app',key:'2'},
+    {text:'Play on the switch',key:'3'}
+  ]);
+  const preesHandler=(key)=>{
+    setTodos((prevTodos)=>{
+      return prevTodos.filter(todo=> todo.key!=key);
+    });
+  }
+  const submtiHandler=(text)=>{
+    setTodos((prevTodos)=>{
+      return[
+        {text:text,key:Math.random().toString()},
+        ...prevTodos
+      ]
+    })
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header/>
+      <View style={styles.content}>
+        <AddTodo submtiHandler={submtiHandler}/>
+        <View style={styles.list}>
+          <FlatList
+          
+          data={todos}
+          renderItem={({item})=>(
+            <TodoItem item={item} preesHandler={preesHandler}/>
+          )}/>
+        </View>
+      </View>
     </View>
   );
 }
@@ -14,7 +45,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  content:{
+    padding:40,
+
+  },
+  list:{
+    marginTop:20,
+  }
 });
